f049e0a7aa5894146dbd92ccacca225d1120ae00
joaopedroAnelli <joaopedroanelli99@gmail.com>
2021-12-20 08:14:49 -0300

    Corrigindo icones

--- src/easy-components/Icon/index.js
+++ src/easy-components/Icon/index.js
@@ -5,7 +5,7 @@ import * as Icons from 'react-icons';
 import { Container } from './styles';
 
 function Icon({ name, size, color, ...rest }) {
-  const icon = React.createElement(Icons[name]);
+  const icon = Icons[name] ? React.createElement(Icons[name]) : '';
   return (
     <Container size={size} color={color} {...rest}>
       {icon}
