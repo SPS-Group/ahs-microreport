3daac4d21415d7c89186e35da4ae2f693b353938
joaopedroAnelli <joaopedroanelli99@gmail.com>
2021-12-20 08:42:33 -0300

    Test passing

--- src/Components/ReportPage/ReportPage.test.tsx
+++ src/Components/ReportPage/ReportPage.test.tsx
@@ -1,13 +1,12 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import ReportPage from './index';
-import settings from '~/tests/assets/report_page_settings';
+import settings from '~/tests/assets/simple_settings';
 
 const parsedSettings = settings.replace(/\t/g, ' ').replace(/\n/g, ' ');
-render(
-  <ReportPage data={[{ id: 1, name: 'John' }]} settings={parsedSettings} />
-);
+render(<ReportPage data={[{ id: 1 }]} settings={parsedSettings} />);
 
 test('should render the table', async () => {
   const table = await screen.findByTestId('json-table');
---
+++ src/tests/assets/simple_settings.ts
@@ -0,0 +1,24 @@
+const settings = `
+  
+
+{
+  "rowKeyField": "id",
+  "pdfFontSize": 8,
+  "pdfOrientation": "portrait",
+  "cellColor": "",
+  "autoRefresh": 15000,
+  "mobileCardTemplate": "",
+  "groups": [],
+  "optionsGroup": [],
+  "footerButtons": [],
+  "columns": [
+    {
+      "name": "id",
+      "title": "ID"
+    }
+  ],
+  "params": []
+}
+`;
+
+export default settings;
