05f9756b04b72a6271ce11c3fc0661d0a9d005e9
joaopedroAnelli <joaopedroanelli99@gmail.com>
2021-12-20 08:18:28 -0300

    Update ReportPage.test.tsx

--- src/Components/ReportPage/ReportPage.test.tsx
+++ src/Components/ReportPage/ReportPage.test.tsx
@@ -1,15 +1,12 @@
+/* eslint-disable no-undef */
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ReportPage from './index';
 import settings from '~/tests/assets/report_page_settings';
 
 const parsedSettings = settings.replace(/\t/g, ' ').replace(/\n/g, ' ');
 render(
-  <ReportPage
-    loggedIn
-    data={[{id: 1, 'name': 'John',}]}
-    columns={[{key: 'id', title: 'ID'}, {key: 'name', title: 'NOME'}]}
-    settings={parsedSettings}
-  />
+  <ReportPage data={[{ id: 1, name: 'John' }]} settings={parsedSettings} />
 );
 
 test('should render the table', async () => {
